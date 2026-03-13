9dae7504a7fc5cceec54ff0f565be0322bd9f6d4
2233admin <1497479966@qq.com>
2026-03-13 15:44:28 +0800

    test: isolate widget smoke test from home side effects

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 import 'package:cicada/main.dart';
 
 void main() {
-  testWidgets('CicadaApp renders', (WidgetTester tester) async {
-    await tester.pumpWidget(const CicadaApp());
-    expect(find.text('\u{1F997} 知了猴'), findsOneWidget);
+  testWidgets('CicadaApp exposes the expected app shell metadata', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const CicadaApp(home: SizedBox.shrink()),
+    );
+
+    final app = tester.widget<MaterialApp>(find.byType(MaterialApp));
+    expect(app.title, '知了猴');
   });
 }
